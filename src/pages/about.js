541a9e296549c0648260303dc15652611d827317
Olga Tkach <tkacholya@gmail.com>
2019-01-23 11:33:29 -0500

    refactored layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,15 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 const AboutPage = () => (
-  <Layout>
+  <Layout pageTitle="About us">
     <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>About us</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate
+      quisquam reiciendis saepe laudantium, hic quidem magnam eius ducimus rem
+      modi tempore dolore consequatur beatae dolor, necessitatibus ex? Obcaecati
+      perferendis repudiandae vel. Laborum laboriosam totam obcaecati harum
+      itaque alias cum iusto.
+    </p>
   </Layout>
 )
 
